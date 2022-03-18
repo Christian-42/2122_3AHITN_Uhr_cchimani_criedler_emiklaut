0bd6d4504d35b46358782d25d86e927fc9655c4b
Christian42 <cchimani@htl-steyr.ac.at>
2022-03-18 08:28:09 +0100

    a

--- src/main/java/com/example/uhrzeit/HelloController.java
+++ src/main/java/com/example/uhrzeit/HelloController.java
@@ -2,8 +2,13 @@ package com.example.uhrzeit;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 
 public class HelloController {
+    public BorderPane Hintergrund;
+    public Label aktuelleUhrzeit;
+    public Label aktuellesDatum;
+    public Label aktuelleKalenderWoche;
     @FXML
     private Label welcomeText;
 
