769104fea5fabbef92e2dd478cb84305b78664ef
emiklaut <emiklaut@htl-steyr.ac.at>
2022-03-18 09:40:54 +0100

    Zeitformat.java programmiert + Zeit.java programmiert + Hintergrund methode erstellt

--- src/main/java/com/example/uhrzeit/view/Zeitformat.java
+++ src/main/java/com/example/uhrzeit/view/Zeitformat.java
@@ -1,9 +1,11 @@
 package com.example.uhrzeit.view;
 
+import java.util.function.IntBinaryOperator;
+
 public class Zeitformat extends Zeit{
-    private int st;
-    private int min;
-    private int sek;
+    int min;
+    int st;
+    int sek;
 
     Zeitformat(int st, int min, int sek) {
         super(st, min, sek);
@@ -34,7 +36,7 @@ public class Zeitformat extends Zeit{
         this.sek = sek;
     }
 
-    public void Zeitformat24(int st, int min, int sek){
+    public void zeitformat24(int st, int min, int sek){
         this.st = st;
         this.min = min;
         this.sek = sek;
@@ -45,7 +47,7 @@ public class Zeitformat extends Zeit{
             gettime();
     }
 
-    public void Zeitformat12(int st, int min, int sek){
+    public void zeitformat12(int st, int min, int sek){
         this.st = st;
         this.min = min;
         this.sek = sek;
@@ -56,13 +58,15 @@ public class Zeitformat extends Zeit{
             System.out.println(st + ":" + min + ":" + sek + "am" );
     }
 
-    public void BinZeitformat(){
+    public void binaryzeitformat(int st, int min, int sek){
+        this.st = st;
+        this.min = min;
+        this.sek = sek;
 
+        System.out.println(Integer.toBinaryString(st)+ ":" + Integer.toBinaryString(min) + ":" + Integer.toBinaryString(sek));
     }
 
-    void ausgabe() {
 
-    }
 }
 
 
