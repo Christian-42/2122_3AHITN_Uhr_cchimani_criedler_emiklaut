d7ab7940c295ac483060b1113889d4e6e1e0bfe2
criedler <criedler@htl-steyr.ac.at>
2022-04-01 09:51:24 +0200

    Menu Button methoden Erstellen neuer Controller Klassen

--- src/main/java/com/example/uhrzeit/controller/HelloController.java
+++ src/main/java/com/example/uhrzeit/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.uhrzeit.controller;
 
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -16,68 +17,86 @@ public class HelloController {
     public Label aktuelleUhrzeit;
     public Label aktuellesDatum;
     public Label aktuelleKalenderWoche;
+    /*
     public Label stoppuhrausgabe;
     public Button stoppuhr;
     public Button startUhr;
+    */
     public BorderPane Hintergrund;
-    @FXML
-    private Label welcomeText;
 
 
-    public void initialize(){
-        aktuellesDatum.setText(ZonedDateTime.now().getDayOfMonth()+"."+ZonedDateTime.now().getMonthValue()+"."+ZonedDateTime.now().getYear());
-        aktuelleUhrzeit.setText(ZonedDateTime.now().getHour()+":"+ZonedDateTime.now().getMinute()+":"+ZonedDateTime.now().getSecond());
-        Calendar calendar=Calendar.getInstance();
-        Date date=new Date();
+    public void initialize() {
+        aktuellesDatum.setText(ZonedDateTime.now().getDayOfMonth() + "." + ZonedDateTime.now().getMonthValue() + "." + ZonedDateTime.now().getYear());
+        aktuelleUhrzeit.setText(ZonedDateTime.now().getHour() + ":" + ZonedDateTime.now().getMinute() + ":" + ZonedDateTime.now().getSecond());
+        Calendar calendar = Calendar.getInstance();
+        Date date = new Date();
         calendar.setTime(date);
         aktuelleKalenderWoche.setText(String.valueOf(calendar.get(Calendar.WEEK_OF_YEAR)));
     }
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    public class UpdateDigital implements Runnable {
+
+        @Override
+        public void run() {
+            while (true){
+                try {
+                    Thread.sleep(1000);
+                    Platform.runLater();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                aktuelleUhrzeit.setText(ZonedDateTime.now().getHour() + ":" + ZonedDateTime.now().getMinute() + ":" + ZonedDateTime.now().getSecond());
+                Calendar calendar = Calendar.getInstance();
+                Date date = new Date();
+                calendar.setTime(date);
+                aktuelleKalenderWoche.setText(String.valueOf(calendar.get(Calendar.WEEK_OF_YEAR)));
+                aktuellesDatum.setText(ZonedDateTime.now().getDayOfMonth() + "." + ZonedDateTime.now().getMonthValue() + "." + ZonedDateTime.now().getYear());
+            }
+        }
+
+        public void main(String[] args) {
+
+        }
+    }
+
+
+    public void hintergrund(BorderPane hintergrund) {
+        this.hintergrund = hintergrund;
     }
 
     public void onStoppUhr_Timer_click(ActionEvent actionEvent) throws InterruptedException {
-        boolean isRunning = true;
-        int sec=0;
-        int min=0;
-        int h=0;
-
-
-
+        new StoppUhr_TimerController();
+        /*boolean isRunning = true;
+        int sec = 0;
+        int min = 0;
+        int h = 0;
 
         if (startUhr.isHover()) {
-            while (h<=59) {
-                stoppuhrausgabe.setText("sec:"+String.valueOf(sec)+" min: "+min+" h: "+h);
+            while (h <= 59) {
+                stoppuhrausgabe.setText("sec:" + String.valueOf(sec) + " min: " + min + " h: " + h);
                 sec++;
                 //Thread.sleep(1000);
-                if (sec==60){
+                if (sec == 60) {
                     min++;
-                    sec=0;
+                    sec = 0;
                 }
-                if (min == 60){
+                if (min == 60) {
                     h++;
-                    min=0;
+                    min = 0;
                 }
-
-
             }
-
         }
-
-
+        */
     }
 
-
-    public void hintergrund (BorderPane hintergrund){
-        this.hintergrund = hintergrund;
-
-    }
-
+    @FXML
     public void onAnalogclick(ActionEvent actionEvent) {
+        new AnalogController();
     }
 
     public void onBinaerclick(ActionEvent actionEvent) {
+        new StoppUhr_TimerController();
     }
 }
 
---
+++ src/main/java/com/example/uhrzeit/controller/StoppUhr_TimerController.java
@@ -0,0 +1,22 @@
+package com.example.uhrzeit.controller;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class StoppUhr_TimerController extends Stage {
+    Stage stage;
+    public void Start() throws IOException {
+        stage = this;
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("StoppUhr_TimerController.fxml")));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}
