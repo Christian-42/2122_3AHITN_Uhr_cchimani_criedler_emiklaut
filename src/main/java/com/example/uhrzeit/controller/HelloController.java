004e7827e2be379de171829a353f18d2a65075e8
Christian42 <cchimani@htl-steyr.ac.at>
2022-04-08 10:13:47 +0200

    a

--- src/main/java/com/example/uhrzeit/controller/HelloController.java
+++ src/main/java/com/example/uhrzeit/controller/HelloController.java
@@ -13,22 +13,16 @@ import java.time.ZonedDateTime;
 import java.util.Calendar;
 import java.util.Date;
 
-public class HelloController{
+public class HelloController {
     public BorderPane hintergrund;
     public Label aktuelleUhrzeit;
     public Label aktuellesDatum;
     public Label aktuelleKalenderWoche;
-    /*
-    public Label stoppuhrausgabe;
-    public Button stoppuhr;
-    public Button startUhr;
-    */
     public BorderPane Hintergrund;
     public Button runde;
 
 
     public void initialize() {
-        System.out.println("init");
 
         aktuellesDatum.setText(ZonedDateTime.now().getDayOfMonth() + "." + ZonedDateTime.now().getMonthValue() + "." + ZonedDateTime.now().getYear());
 
@@ -38,84 +32,57 @@ public class HelloController{
         aktuelleKalenderWoche.setText(String.valueOf(calendar.get(Calendar.WEEK_OF_YEAR)));
 
 
+        Thread thread = new Thread(new Runnable() {
 
-            Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Runnable updater = new Runnable() {
 
-                @Override
-                public void run() {
-                    Runnable updater = new Runnable() {
-
-                        @Override
-                        public void run() {
-                            aktuelleUhrzeit.setText(ZonedDateTime.now().getHour() + ":" + ZonedDateTime.now().getMinute() + ":" + ZonedDateTime.now().getSecond());
-                        }
-                    };
-
-                    while (true) {
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException ex) {
-                        }
-
-                        // UI update is run on the Application thread
-                        Platform.runLater(updater);
-
+                    @Override
+                    public void run() {
+                        aktuelleUhrzeit.setText(ZonedDateTime.now().getHour() + ":" + ZonedDateTime.now().getMinute() + ":" + ZonedDateTime.now().getSecond());
                     }
-                }
+                };
 
-            });
-            // don't let thread prevent JVM shutdown
+                while (true) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                    }
+
+                    // UI update is run on the Application thread
+                    Platform.runLater(updater);
+
+                }
+            }
+
+        });
+        // don't let thread prevent JVM shutdown
         thread.setDaemon(true);
         thread.start();
-        }
+    }
 
 
+    public void hintergrund(BorderPane hintergrund) {
+        this.hintergrund = hintergrund;
+    }
 
-            public void hintergrund(BorderPane hintergrund) {
-                this.hintergrund = hintergrund;
-            }
+    public void onStoppUhr_Timer_click(ActionEvent actionEvent) throws InterruptedException, IOException {
+        System.out.println("received click");
+        new StoppUhr_TimerApplication();
+    }
 
-            public void onStoppUhr_Timer_click(ActionEvent actionEvent) throws InterruptedException, IOException {
-                System.out.println("received click");
-                new StoppUhr_TimerApplication();
-        /*boolean isRunning = true;
-        int sec = 0;
-        int min = 0;
-        int h = 0;
+    @FXML
+    public void onAnalogclick(ActionEvent actionEvent) throws IOException {
+        new AnalogApplication();
+    }
 
-        if (startUhr.isHover()) {
-            while (h <= 59) {
-                stoppuhrausgabe.setText("sec:" + String.valueOf(sec) + " min: " + min + " h: " + h);
-                sec++;
-                //Thread.sleep(1000);
-                if (sec == 60) {
-                    min++;
-                    sec = 0;
-                }
-                if (min == 60) {
-                    h++;
-                    min = 0;
-                }
-            }
-        }
-        */
-            }
-
-            @FXML
-            public void onAnalogclick(ActionEvent actionEvent) throws IOException {
-                new AnalogApplication();
-            }
-
-            public void onBinaerclick(ActionEvent actionEvent) throws IOException {
-                new BinaerApplication();
-            }
+    public void onBinaerclick(ActionEvent actionEvent) throws IOException {
+        new BinaerApplication();
+    }
 
     public void onswitchformatclick(ActionEvent actionEvent) {
 
     }
 }
-
-
-
-
 
--- src/main/java/com/example/uhrzeit/controller/StoppUhr_TimerController.java
+++ src/main/java/com/example/uhrzeit/controller/StoppUhr_TimerController.java
@@ -1,4 +1,80 @@
 package com.example.uhrzeit.controller;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.*;
+
 public class StoppUhr_TimerController {
+    public Button runde;
+    public Button stoppstart;
+    public Label ausgabefeldstoppuhr;
+    public Label timeausgabe;
+    public Button abbrechen;
+    public Button startpause;
+    public TextField eingabedergewuenchtenzeit;
+    public Label timeausgabe1;
+    public Label timerabgelaufensymbol;
+    static int eingabe = 0;
+    public ImageView imageview;
+
+    public void startpause(ActionEvent actionEvent) throws InterruptedException {
+
+
+
+        eingabe= Integer.valueOf(eingabedergewuenchtenzeit.getText());
+        System.out.println(eingabe);
+
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                Runnable updater = new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                        eingabe--;
+                        timeausgabe1.setText(String.valueOf(eingabe));
+                    }
+                };
+
+                while (eingabe>1) {
+
+                    if (eingabe==0){
+                        File file = new File("com/example/resources/symbole-glocke.jpg");
+                        Image image = new Image(file.toURI().toString());
+                        imageview = new ImageView(image);
+
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                    }
+
+                    // UI update is run on the Application thread
+                    Platform.runLater(updater);
+
+                }
+            }
+
+
+        });
+        // don't let thread prevent JVM shutdown
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+
 }
